bc854247080e85c08207a2fd8dc249b3275fa930
Paul Angel <pwangel@5000L-138115-M.local>
2021-02-11 15:25:29 +1100

    Script to generate REIN input files from data and gene list (randomised gene relationships)

--- notebooks/boolean_analysis/generate_REIN_model.py
+++ notebooks/boolean_analysis/generate_REIN_model.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-import scipy, sys, sklearn.mixture
+import scipy, sys, sklearn.mixture, sklearn.cluster
 sys.path.append('/Users/pwangel/pauls_atlas_building/notebooks/')
 import functions
 
@@ -60,12 +60,13 @@ annotations = pd.read_csv('/Users/pwangel/Downloads/RNASeq_pluripotent_annotatio
 genes       = pd.read_csv('/Users/pwangel/Data/ensembl_hg38.91/gene_to_symbol_ensembl91_human.tsv', sep='\t', index_col=0, names=['symbol'])
 
 annotations.index = [i+';'+j for i,j in zip(annotations.chip_id.values.astype(str), annotations.Dataset.values.astype(int).astype(str))]
-#data        = data[annotations.index]
 data        = data[annotations.chip_id]
 
 data = functions.transform_to_percentile(data)
 
-gmm = sklearn.mixture.GaussianMixture(n_components=2)
+kmeans = sklearn.cluster.KMeans(n_clusters=2)
+
+data_output = pd.DataFrame(index=gene_list, columns=['Bimodal val', 'Low_Expression', 'High_Expression', 'Low_Std', 'High_Std'])
 
 #Loop through the sample types and genes to find differentially expressed genes
 for i_gene in gene_list:
@@ -77,32 +78,105 @@ for i_gene in gene_list:
     if i_gene == 'ZFP57':
         ensembl_id = ['ENSG00000204644']
 
-    gmm.fit(data.loc[ensembl_id].values.reshape(-1, 1))
-    prediction = gmm.predict(data.loc[ensembl_id].values.reshape(-1, 1))
-    n_ambiguous_samples = ((gmm.predict_proba(data.loc[ensembl_id].values.reshape(-1, 1))[:,0] >= 0.25) & \
-                                         (gmm.predict_proba(data.loc[ensembl_id].values.reshape(-1, 1))[:,0] < 0.75)).sum()
+    
+    kmeans.fit(data.loc[ensembl_id].values.reshape(-1, 1))
+    prediction = kmeans.predict(data.loc[ensembl_id].values.reshape(-1, 1))
 
-    #delta_mean = data.loc[ensembl_id,prediction==0].values.mean()-data.loc[ensembl_id,prediction==1].values.mean()
-    #std_sum    = data.loc[ensembl_id,prediction==0].values.std()+data.loc[ensembl_id,prediction==1].values.std()
-    delta_mean = data.loc[ensembl_id,annotations.LM_Group_COLOR.values=='naive'].values.mean()-data.loc[ensembl_id,annotations.LM_Group_COLOR.values=='primed'].values.mean()
-    std_sum    = data.loc[ensembl_id,annotations.LM_Group_COLOR.values=='naive'].values.std()+data.loc[ensembl_id,annotations.LM_Group_COLOR.values=='primed'].values.std()
+    delta_mean = data.loc[ensembl_id,prediction==0].values.mean()-data.loc[ensembl_id,prediction==1].values.mean()
+    std_sum    = data.loc[ensembl_id,prediction==0].values.std()+data.loc[ensembl_id,prediction==1].values.std()
 
-    print(i_gene, ensembl_id, n_ambiguous_samples, np.abs(delta_mean)/std_sum)
+    low_exp_group = np.argmin([data.loc[ensembl_id,prediction==0].values.mean(),data.loc[ensembl_id,prediction==1].values.mean()])
 
-    fig = Figure()
-    fig.add_trace(Histogram(x=data.loc[ensembl_id, prediction==0].values[0], name='Group 0',    
-            xbins=dict(
-                       start=0.0,
-                       end=1.0,
-                       size=0.025)
-            ))
+    data_output.loc[i_gene, 'Bimodal val'] = np.abs(delta_mean)/std_sum
+    data_output.loc[i_gene, 'Low_Expression'] = data.loc[ensembl_id,prediction==low_exp_group].values.mean()
+    data_output.loc[i_gene, 'High_Expression'] = data.loc[ensembl_id,prediction!=low_exp_group].values.mean()
+    data_output.loc[i_gene, 'Low_Std'] = data.loc[ensembl_id,prediction==low_exp_group].values.std()
+    data_output.loc[i_gene, 'High_Std'] = data.loc[ensembl_id,prediction!=low_exp_group].values.std()
 
-    fig.add_trace(Histogram(x=data.loc[ensembl_id, prediction==1].values[0], name='Group 1',    
-            xbins=dict(
-                       start=0.0,
-                       end=1.0,
-                       size=0.025)
-            ))
-    fig.update_layout(barmode='overlay')
-    fig.update_traces(opacity=0.5)
-    #plot(fig)
+#only want genes that show some bimodality
+data_output = data_output.loc[data_output['Bimodal val']>0.9]
+
+condition_dataframe = pd.DataFrame(index=data_output.index)
+for i_dataset in annotations.Dataset.unique():
+    for j_condition in annotations.loc[annotations.Dataset==i_dataset]['Experiment'].unique():
+        for k_celltype in annotations.loc[(annotations.Dataset==i_dataset)&(annotations.Experiment==j_condition)].LM_Group_COLOR.unique():
+            i_name = '_'.join([str(i_dataset),str(j_condition),str(k_celltype)])
+            sel    = (annotations.Dataset.values == i_dataset) & (annotations.Experiment.values==j_condition) & (annotations.LM_Group_COLOR.values==k_celltype)
+            condition_dataframe[i_name] = np.nan
+
+            for i_gene in data_output.index.values:
+
+                ensembl_id = genes.index.values[genes.symbol.values==i_gene]
+
+                if i_gene == 'POU5F1':
+                    ensembl_id = ['ENSG00000204531']
+                if i_gene == 'ZFP57':
+                    ensembl_id = ['ENSG00000204644']
+
+                if np.abs(data.loc[ensembl_id, sel].values.mean()-data_output.loc[i_gene].Low_Expression)/data_output.loc[i_gene].Low_Std < \
+                   np.abs(data.loc[ensembl_id, sel].values.mean()-data_output.loc[i_gene].High_Expression)/data_output.loc[i_gene].High_Std:
+                    condition_dataframe.loc[i_gene, i_name] = 0
+                else:
+                    condition_dataframe.loc[i_gene, i_name] = 1
+
+#Open two files to place experimental conditions into, we will append one to the other afterwards
+f_experiments  = open('/Users/pwangel/Downloads/temp/temp_experiments.txt', 'a') 
+f_culture_cond = open('/Users/pwangel/Downloads/temp/temp_conditions.txt', 'a')
+
+#Print out results into text files for loading into REIN
+experiment_counter=0
+for i_dataset in annotations.Dataset.unique():
+    sel_conditions = (annotations.Dataset==i_dataset)&~pd.isnull(annotations.Experiment)&(annotations.Time>0)
+    for j_condition, initial_condition in zip(annotations.loc[sel_conditions].groupby(['Experiment', 'Initial Condition']).size().reset_index()['Experiment'].values, \
+                                              annotations.loc[sel_conditions].groupby(['Experiment', 'Initial Condition']).size().reset_index()['Initial Condition'].values):
+
+        print(j_condition)
+        f_experiments.write('#Experiment%d[20] |= $%s "";\n' %(experiment_counter,j_condition))
+        if initial_condition!='None':
+            f_experiments.write('#Experiment%d[0] |= $%s "";\n' %(experiment_counter, initial_condition))
+        f_experiments.write('\n')
+
+        f_culture_cond.write('$%s :=\n' %j_condition)
+        f_culture_cond.write('{\n')
+        for i_gene in condition_dataframe.index.values:
+            f_culture_cond.write(' %s = %d\n' %(i_gene, condition_dataframe.loc[i_gene,'_'.join([str(i_dataset),str(j_condition),str('naive')])]))
+        f_culture_cond.write('};\n')
+        f_culture_cond.write('\n')
+
+        experiment_counter+=1
+
+for initial_condition in annotations['Initial Condition'].dropna().unique():
+
+    if initial_condition!='None':
+        f_culture_cond.write('$%s :=\n' %(initial_condition))
+        f_culture_cond.write('{\n')
+        for i_gene in condition_dataframe.index.values:
+            annotations_row = annotations.loc[annotations['Experiment']==initial_condition]
+            f_culture_cond.write(' %s = %d\n' %(i_gene, condition_dataframe.loc[i_gene,'_'.join([str(i_dataset),str(annotations_row.Experiment.unique()[0]),str(annotations_row.LM_Group_COLOR.unique()[0])])]))
+        f_culture_cond.write('};\n')
+        f_culture_cond.write('\n')
+
+f_experiments.write('\n')
+
+f_experiments.close()
+f_culture_cond.close()
+
+f = open("/Users/pwangel/Downloads/temp/observations.txt", "w")
+for tempfile in ['/Users/pwangel/Downloads/temp/temp_experiments.txt', '/Users/pwangel/Downloads/temp/temp_conditions.txt']:
+    f.write(open(tempfile).read())
+f.close()
+
+f_model = open('/Users/pwangel/Downloads/temp/model.txt', 'a')
+
+f_model.write('directive updates sync;\n')
+f_model.write('directive length 20;\n')
+f_model.write('directive uniqueness interactions;\n')
+f_model.write('directive limit 0;\n')
+f_model.write('directive regulation legacy;\n')
+'[](0..15); '.join([str(i_gene) for i_gene in gene_list])+';\n'
+for i_gene in gene_list:
+    for j_gene in gene_list:
+        direction = np.random.choice(['positive', 'negative', 'positive optional', 'negative optional'])
+        f_model.write('%s\t%s\t%s;\n' %(i_gene, j_gene, direction))
+
+f_model.close()
